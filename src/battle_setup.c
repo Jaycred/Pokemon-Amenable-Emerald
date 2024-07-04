1019320f943473219dfe2f7afea76e62c30d6cdd
Jaycred <jayjanbird@gmail.com>
2024-07-04 00:53:53 -0400

    set up Rabble Ranger encounter music

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1488,6 +1488,9 @@ void PlayTrainerEncounterMusic(void)
         case TRAINER_ENCOUNTER_MUSIC_RICH:
             music = MUS_ENCOUNTER_RICH;
             break;
+        case TRAINER_ENCOUNTER_MUSIC_ROCKET:
+            music = MUS_RG_ENCOUNTER_ROCKET;
+            break;
         default:
             music = MUS_ENCOUNTER_SUSPICIOUS;
         }

f15218e622bc6a9634442cf7f6b457a7a80eecb5
Jaycred <jayjanbird@gmail.com>
2024-08-03 12:42:07 -0400

    change shiny odds to 1/256

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -91,7 +91,7 @@
 #define MAX_STAT_STAGE    12
 
 // Shiny odds
-#define SHINY_ODDS 64 // Actual probability is SHINY_ODDS/65536
+#define SHINY_ODDS 256 // Actual probability is SHINY_ODDS/65536 (1/256 in this case)
 
 // Ribbon IDs used by TV and Pokénav
 #define CHAMPION_RIBBON       0
